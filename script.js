c34dc199af0f4b02c36a16bf4a78430b50081c85
helloyanis <helloyanis@hotmail.com>
2024-03-15 16:10:50 +0100

    pwa

--- script.js
+++ script.js
@@ -58,9 +58,12 @@ function handleForm(event) {
 const registerServiceWorker = async () => {
   if ("serviceWorker" in navigator) {
     try {
-      const registration = await navigator.serviceWorker.register("/worker.js", {
-        scope: "/",
-      });
+      const registration = await navigator.serviceWorker.register(
+        "/Rucoy-ScamSnipe/worker.js",
+        {
+          scope: "/Rucoy-ScamSnipe//",
+        }
+      );
       if (registration.installing) {
         console.log("Service worker installing");
       } else if (registration.waiting) {
